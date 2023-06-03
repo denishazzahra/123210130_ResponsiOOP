7dbe6ab7d3a44a922358c64853f0827da43c9f82
Denisha Kyla Azzahra <95396313+denishazzahra@users.noreply.github.com>
2023-06-03 19:27:25 +0700

    Update AlMaulModel.java

--- Responsi/src/modelresponsi/AlMaulModel.java
+++ Responsi/src/modelresponsi/AlMaulModel.java
@@ -14,7 +14,7 @@ import java.sql.*;
 public class AlMaulModel extends Connector implements PriceInterface {
     public String checkLogin(String user, String pass){
         try{
-            String query="SELECT role from accounts where username='"+user+"' and password='"+pass+"'";
+            String query="SELECT role from accounts where username='"+user+"' and password=BINARY '"+pass+"'";
             statement=connection.createStatement();
             ResultSet rs=statement.executeQuery(query);
             while(rs.next()){
